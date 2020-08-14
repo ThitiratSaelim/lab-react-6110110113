371d5ccfa805fc456ba12d974204901e4360ef02
ThitiratSaelim <Lenovo@LAPTOP-3PSGHFMC.(none)>
2020-08-15 03:28:25 +0700

    part 7 - Add random word

--- src/App.js
+++ src/App.js
@@ -5,14 +5,32 @@ import WordCard from './WordCard';
 const word = "Hello";
 
 function App() {
- 
+  var word = "";
+  var rand = Math.floor(Math.random()*6)+1;
+  if(rand == 1) {
+    word = "Hello";
+  }
+  else if(rand == 2){
+    word = "Computer";   
+  }
+  else if(rand == 3){
+    word = "Engineer";
+  }
+  else if(rand == 4){
+    word = "Faculty";
+  }
+  else if(rand == 5){
+    word = "Logic";
+  }
+  else if(rand == 6){
+    word = "Depart";
+  }
   return (
     <body>
       
       <center><br></br><h1>Welcome to Game!</h1><br></br></center>
       <h2>Click the random button to change question!</h2>
-      <button onclick="myFunction()" className="button button1"><a href="http://localhost:3000/">Random</a></button>
-      <p id="demo"></p>
+      <button className="button button1"><a href="http://localhost:3000/" >Random</a></button>
       <div>
         <WordCard value={word}/>
        
