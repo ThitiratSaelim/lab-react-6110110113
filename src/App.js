2665fd9cf7853a27af7673725a0eec5b9ac75f26
ThitiratSaelim <Lenovo@LAPTOP-3PSGHFMC.(none)>
2020-08-15 03:11:00 +0700

    part 7 - random with button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import './App.css';
 import WordCard from './WordCard';
 
 const word = "Hello";
+
 function App() {
+ 
   return (
     <body>
+      
       <center><br></br><h1>Welcome to Game!</h1><br></br></center>
-      <button className="button button1">Random</button><button className="button button2">Next</button>
+      <h2>Click the random button to change question!</h2>
+      <button onclick="myFunction()" className="button button1"><a href="http://localhost:3000/">Random</a></button>
+      <p id="demo"></p>
       <div>
         <WordCard value={word}/>
-      </div>
+       
+      </div> 
       
     </body>
   );
